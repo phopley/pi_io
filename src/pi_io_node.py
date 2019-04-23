7e51f8b7691888009222e67ceb91085dc5aaea2d
phopley <philip@thehopleys.me.uk>
2019-04-23 13:45:55 +0100

    More development

--- src/pi_io_node.py
+++ src/pi_io_node.py
@@ -25,9 +25,10 @@ class PiGpioNode:
     def __init__(self):
         self.__output0 = 23       # GPIO Pin 23 is output index 0
         self.__output1 = 24       # GPIO Pin 23 is output index 1
-        self.__input0 = 22        # GPIO Pin 22 is input index 0
-        self.__input1 = 27        # GPIO Pin 27 is input index 1
+        self.__input0 = 27        # GPIO Pin 27 is input index 0
+        self.__input1 = 22        # GPIO Pin 22 is input index 1
         
+        GPIO.setwarnings(False)
         GPIO.setmode(GPIO.BCM)    # Use GPIO pin number not the physical pin numbering
         
         GPIO.setup(self.__output0, GPIO.OUT)
@@ -42,21 +43,23 @@ class PiGpioNode:
         self.__service = rospy.Service('gpio/output_cmd', gpio_output, self.OutputCommand)        
         self.__gpi_pub = rospy.Publisher('gpio/input_cmd', gpio_input, queue_size=3)
         
-    def OutputCommand(request):
+    def OutputCommand(self, request):
         if(request.index == 0):
-            gpo = self.__output0
+            gpo = self.__output0            
         elif(request.index == 1):
-            gpo = self.__output1
+            gpo = self.__output1            
             
         GPIO.output(gpo, request.value)
+        
+        return True
       
-    def Input0HighCallback(channel):
+    def Input0HighCallback(self, channel):
         input_cmd = gpio_input()
         input_cmd.index = 0
         input_cmd.value = True
         self.__gpi_pub.publish(input_cmd)
 
-    def Input1HighCallback(channel):
+    def Input1HighCallback(self, channel):
         input_cmd = gpio_input()
         input_cmd.index = 1
         input_cmd.value = True
